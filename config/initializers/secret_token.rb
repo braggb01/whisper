c319d26ae8129d21ac09da93ade81746fd589e55
Brian Bragg <brian.bragg@mssm.edu>
2013-01-03 13:44:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whisper::Application.config.secret_token = 'e32c2dbf306ab966eeb9ae633bc416c4c0b198e9b08388d01b462081a49b9cc7d6675cb2d298e809e42d10f9effb0d417bec4299291b7380ea593a554bae8823'
