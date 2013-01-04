d54ee4048cbbe9598ee2d37e43ed27389ca543a0
Brian Bragg <brian.bragg@mssm.edu>
2013-01-04 16:23:26 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Whisper::Application.routes.draw do
 
   resources :users
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   match '/signup', to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/help', to: 'static_pages#help'
 
